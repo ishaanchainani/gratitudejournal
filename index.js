7f17c58ff83c58662c8fb7a52c4e4673ffffc96c
ishaanchainani <59625730+ishaanchainani@users.noreply.github.com>
2021-10-31 17:37:49 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,50 +1,51 @@
 import Head from 'next/head'
 import Greeting from '../components/Greeting'
+import History from '../components/History'
 import Input from "../components/Input";
 import { useState } from "react";
+import { Auth } from '@supabase/ui'
 
+
+import { supabase } from "../utils/supabaseClient"
+import GratitudeApp from '../components/GratitudeApp';
 
 export default function Home() {
-
-  const [user, setUser] = useState({
-        name: "Ishaan",
-        email: "chainani@chapman.edu",
-      });
-    
-      const [gratitudes, setGratitudes] = useState([]);
-    
-      const [hasSubmittedToday, setHasSubmittedToday] = useState(false);
-    
-      const addGratitude = (entry) => {
-        let newGratitude = [...gratitudes, entry];
-        setGratitudes(newGratitude);
-        setHasSubmittedToday(true);
-      };
+  const {user} = Auth.useUser()
 
     return (
           <div className="bg-gray-700 min-h-screen min-w-screen">
             <Head>
-              <title>Hello</title>
+              <title>GratitudeApp</title>
               <link rel="icon" href="/favicon.ico" />
             </Head>
       
-            <main className="flex flex-col p-1 items-center">
-              <Greeting
-                color="text-pink-800"
-                user={user}
-                gratitudes={gratitudes}
-                hasSubmittedToday={hasSubmittedToday}
-              ></Greeting>
-              {!hasSubmittedToday && <Input handleSubmit={addGratitude}></Input>}
+            <main className="container mx-auto max-w-prose px-4 pt-12">
+          
+              {
+
+                user ? (
+                  <GratitudeApp user={user} />
+                )   : (
+
+
+                  <div className="bg-white">
+                  <Auth supabaseClient = {supabase} socialLayout = "horizontal" socialButtonSize ="xlarge"/>
+                  </div>
+                )
+                  
+                          
+
+              }
+
+
+              
+
+            </main>
+
+         
+           </div>
+        )
+
       
-              {gratitudes.length > 0 && (
-                <History
-                  className="p-3"
-                  gratitudes={gratitudes}
-                 
-                ></History>
-              )}
-            </main>
-          </div>
-        );
-}
+     
+    }
