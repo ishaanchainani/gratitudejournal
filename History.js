b0901b7a8ca18574d1b51ca8f34402ef0341c7ce
ishaanchainani <59625730+ishaanchainani@users.noreply.github.com>
2021-10-31 17:37:06 -0700

    Update History.js

--- History.js
+++ History.js
@@ -1,14 +1,15 @@
 export default function History({ gratitudes }) {
       return (
-        <div className="text-white text-6xl p-6">
-          <h2 className="font-black">Previous gratitudes: </h2>
+        <p className="text-white text-6xl p-6">
+          
+            You were previously grateful for: 
     
-          <span className="flex flex-col p-5 items-center">
-            {gratitudes.map((g) => (
-              <div>{gratitudes.toString()}
-                        </div>
-            ))}
+          <span className="font-bold">
+            {gratitudes.map((g) => ' ' + g.entry) .toString() }(
+             
           </span>
-        </div>
+        </p>
       );
     }
+
+    
