0e304c7ea8f64e363c0d12ea5f6746f345b7e45a
ishaanchainani <59625730+ishaanchainani@users.noreply.github.com>
2021-10-31 18:01:26 -0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,16 @@
+import 'tailwindcss/tailwind.css'
+import { Auth } from '@supabase/ui'
+import { supabase } from "../utils/supabaseClient"
+
+function MyApp({ Component, pageProps }) {
+  return(
+    <Auth.UserContextProvider supabaseClient={supabase}>
+      <Component {...pageProps} />
+
+    </Auth.UserContextProvider>
+  ) 
+  }
+
+export default MyApp
+
+  
